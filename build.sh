6c06c6d0d4ed94544ccf190934e5fcdb948724ad
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2024-08-21 14:12:53 +0200

    build: update build and container workflow

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 #sudo apt install libasio-dev git -y
 git clone https://github.com/CrowCpp/Crow --depth 1
-wclean src/
 wmakeLnInclude src/
-wclean interactiveUI/
-wmakeLnInclude interactiveUI/
-wmake interactiveUI
-npm install --prefix reflect-json-app/ 
+wmake src/testlib
+wclean applications/endpoint
 cd TUI || exit 1
 go build -o tui main.go
 #npm start --prefix reflect-json-app/
