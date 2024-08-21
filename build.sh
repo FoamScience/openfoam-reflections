82bd6c3292c14a0ec136e47c840ca37d2135c631
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2024-08-21 14:19:01 +0200

    build: minor fixes for build scripts

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 #sudo apt install libasio-dev git -y
 git clone https://github.com/CrowCpp/Crow --depth 1
 wmakeLnInclude src/
+wclean src/testlib
 wmake src/testlib
 wclean applications/endpoint
+wmake applications/endpoint
 cd TUI || exit 1
 go build -o tui main.go
 #npm start --prefix reflect-json-app/
